a3cb7b1e04e5cc426169f155f7d8739846d71179
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-09-23 17:44:31 +0200

    add better error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/state';
+</script>
+
+<main>
+	<section class="bg-hero p-16 shadow-md">
+		<div class="mx-auto max-w-6xl">
+			<div class="flex flex-col items-center justify-center gap-12 text-white">
+				<p class="text-4xl">{page.error?.message}</p>
+				<a class="link" href="https://mupen64.com">Back to the Homepage</a>
+			</div>
+		</div>
+	</section>
+</main>
