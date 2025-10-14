4b7bb5784029ad4dacd8308b4913368e404daa5b
Aurumaker72 <48759429+Aurumaker72@users.noreply.github.com>
2025-10-14 18:16:12 +0200

    add download button to sm64lua redux, bump version

--- src/routes/sm64luaredux/+page.svelte
+++ src/routes/sm64luaredux/+page.svelte
@@ -7,13 +7,19 @@
 	<Hero
 		name="SM64 Lua Redux"
 		description="Powerful SM64 TASing utility."
-		version="5.0.0"
+		version="5.0.1"
 		logo={sm64luaredux}
 		repository="https://github.com/mupen64/SM64LuaRedux"
 		bg_color_from="#ffffff"
 		bg_color_to="#dddddd"
 		dark={false}
-	></Hero>
+	>
+		{#snippet button()}
+			<a href="https://github.com/mupen64/SM64LuaRedux/releases/latest" class="button primary bg-gray-600! shadow-black! text-white!"
+				>Download</a
+			>
+		{/snippet}
+	</Hero>
 
 	<section id="about" class="mb-12 p-16 pb-0">
 		<div class="mx-auto max-w-6xl">
