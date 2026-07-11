fb6ea08e44954998a76082f4ce9b9f638d073131
abart27 <48759429+abart27@users.noreply.github.com>
2026-07-11 16:49:24 +0200

    feat: background color for docs

--- src/routes/docs/[product]/[channel]/[...slug]/+page.svelte
+++ src/routes/docs/[product]/[channel]/[...slug]/+page.svelte
@@ -7,7 +7,7 @@
 
 <main>
 	<section id="wiki" class="mx-auto max-w-6xl p-4 py-16">
-		<div class="mx-auto mb-8 flex max-w-4xl flex-col items-start gap-4 sm:flex-row sm:items-end sm:justify-between">
+		<div class="mx-auto mb-8 flex max-w-4xl flex-col items-start gap-4 rounded-box border border-base-300 bg-base-300 p-6 shadow-sm sm:flex-row sm:items-end sm:justify-between">
 			<div>
 				<p class="flex items-center gap-2 text-sm uppercase tracking-wide opacity-70">
 					<img
@@ -37,7 +37,7 @@
 			</div>
 		</div>
 
-		<div class="mx-auto flex max-w-4xl flex-col items-start">
+		<div class="mx-auto flex max-w-4xl flex-col items-start rounded-box border border-base-300 bg-base-200 p-6 shadow-sm">
 			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 			{@html data.content}
 		</div>
