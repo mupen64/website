c59bec2b4e2a15b5c28ef88c8b0db2209e1bca07
abart27 <48759429+abart27@users.noreply.github.com>
2026-07-11 14:26:22 +0200

    fix: wrong doc name normalization

--- src/lib/helpers/DocNameConverter.ts
+++ src/lib/helpers/DocNameConverter.ts
@@ -1,6 +1,6 @@
 export function doc_name_to_friendly_name(name: string) {
-	const baseName = name.split('/').pop() ?? name;
-	const friendlyName = baseName.replace(/^\d+\.\s*/, '').split('_').join(' ');
+  const baseName = name.split('/').pop() ?? name;
+  const friendlyName = baseName.replace(/^\d+\.\s*/, '').replaceAll('_', ' ').replaceAll('-', ' ');
 
-	return friendlyName.charAt(0).toUpperCase() + friendlyName.slice(1);
+  return friendlyName.charAt(0).toUpperCase() + friendlyName.slice(1);
 }
