02687e534ac4a1e29ab71c5d301b8db32c6099a8
abart27 <48759429+abart27@users.noreply.github.com>
2026-07-11 14:14:42 +0200

    fix: doc name handling

--- src/lib/helpers/DocNameConverter.ts
+++ src/lib/helpers/DocNameConverter.ts
@@ -1,6 +1,6 @@
 export function doc_name_to_friendly_name(name: string) {
 	const baseName = name.split('/').pop() ?? name;
-	const friendlyName = baseName.replace(/^\d+\.\s*/, '').split('-').join(' ');
+	const friendlyName = baseName.replace(/^\d+\.\s*/, '').split('_').join(' ');
 
 	return friendlyName.charAt(0).toUpperCase() + friendlyName.slice(1);
 }
