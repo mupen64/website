e65a31903a64e7d4c2e3a5c0bb418bfddee92eec
abart27 <aurumaker@proton.me>
2026-09-23 18:46:43 +0200

    fix: 404 on download pages after reload

--- src/routes/downloaded/[channel]/+page.ts
+++ src/routes/downloaded/[channel]/+page.ts
@@ -1,4 +1,3 @@
-export const entries = () =>
-	(['stable-w32', 'stable-w64', 'nightly-w32', 'nightly-w64'] as const).map((channel) => ({
-		channel
-	}));
+import { downloadUrls } from '$lib/helpers/RepackDownloadHelper';
+
+export const entries = () => Object.keys(downloadUrls).map((channel) => ({ channel }));
